096304b4a8a284b426b2ff936f92f2530722ef79
Thyago Cabral de Sousa <thyago.tcs@dpf.gov.br>
2022-03-06 12:01:17 -0300

    Adicionando configuraçoes iniciais para o projeto

---
+++ android/app/src/main/java/com/thyagodevel_rn_firebase_login/MainActivity.java
@@ -0,0 +1,15 @@
+package com.thyagodevel_rn_firebase_login;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "thyagodevel_RN_firebase_login";
+  }
+}
